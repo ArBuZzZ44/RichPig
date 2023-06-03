cd2e6811e72c41d18398da080dda1ec1d34847a8
ArBuZzZ44 <zahar1702@mail.ru>
2023-06-04 05:49:02 +0700

    decorate capitals

--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -1,13 +1,14 @@
 class AdditionsController < ApplicationController
   def create 
     @capital = Capital.find params[:capital_id]
-    @addition = @capital.additons.build addition_params
+    @addition = @capital.additions.build addition_params
 
     if @addition.save 
       flash[:success] = "You have replenished the piggy bank"
       redirect_to capital_path(@capital)
     else
       @additions = Addition.order created_at: :desc
+      @capital = @capital.decorate
       render "capitals/show"
     end
   end
