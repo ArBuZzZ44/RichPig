7f60110d300853333378dd31d861888dbe5dfa08
ArBuZzZ44 <zahar1702@mail.ru>
2023-06-04 05:35:33 +0700

    add method create

--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -1,3 +1,20 @@
 class AdditionsController < ApplicationController
-  
+  def create 
+    @capital = Capital.find params[:capital_id]
+    @addition = @capital.additons.build addition_params
+
+    if @addition.save 
+      flash[:success] = "You have replenished the piggy bank"
+      redirect_to capital_path(@capital)
+    else
+      @additions = Addition.order created_at: :desc
+      render "capitals/show"
+    end
+  end
+
+  private 
+
+  def addition_params
+    params.require(:addition).permit(:balance)
+  end
 end
