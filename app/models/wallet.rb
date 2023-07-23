dc4399d53ca09832f4656ede02c211b21e547b67
ArBuZzZ44 <zahar1702@mail.ru>
2023-07-24 06:20:06 +0700

    added validation

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,8 @@
 class Wallet < ApplicationRecord
   has_many :profits, dependent: :destroy
   has_many :payments, dependent: :destroy
+
+	validates :budget, presence: true, numeric: true
+	validates :start_date, presence: true
+	validates :end_date, presence: true, comparison: {greater_than: :start_date}
 end
