d7754af5263d66c5ac2db7cce39ecdbfb55cd2e2
ArBuZzZ44 <zahar1702@mail.ru>
2023-07-30 04:57:09 +0700

    added validations for operation model

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,7 @@
 class Operation < ApplicationRecord
   belongs_to :wallet
+
+	validates :operation_type, presence: true, inclusion: { in: %w(Expense Profit) }
+	validates :operation_amount, presence: true, numeric: true
+	validates :operation_date, presence: true
 end
