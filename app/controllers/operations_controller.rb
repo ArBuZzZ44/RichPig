4de01bf82a08bdf4ac55385b2cf5453a57979eaf
ArBuZzZ44 <zahar1702@mail.ru>
2023-07-30 05:04:54 +0700

    added method create

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,2 +1,20 @@
 class OperationsController < ApplicationController
+	def create
+		@wallet = Wallet.find params[:wallet_id]
+    @operation = @wallet.operations.build operation_params
+
+		if @operation.save
+			flash[:success] = "The operation was successful"
+			redirect_to wallet_path(@wallet)
+		else
+			@operations = @wallet.operations.order created_at: :desc
+			render "wallets/show"
+		end
+	end
+
+	private 
+
+	def operation_params
+		params.require(:operation).permit(:operation_amount, :operation_date, :operation_type)
+	end
 end
