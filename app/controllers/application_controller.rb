b10f69fd50af3c1edcd69e99501675e6aef633db
ArBuZzZ44 <zahar1702@mail.ru>
2023-09-11 04:56:28 +0700

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::Base
-	around_action :switch_locale
-
-	private
-
-	def switch_locale(&action)
-		locale = localee_from_url || I18n.default_locale
-		I18n.with_locale(locale, &action)
-	end
-
-	def localee_from_url
-		locale = params[:locale]
-
-		return locale if I18n.available_locales.map(&:to_s).include?(locale)
-	end
-
-	def default_url_options
-		{ locale: I18n.locale }
-	end
+	include Internationalization
 end
