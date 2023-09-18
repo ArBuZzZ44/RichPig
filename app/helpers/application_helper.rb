bf6082403ce038760acd0973f36a8f298f8813b4
ArBuZzZ44 <zahar1702@mail.ru>
2023-09-19 02:12:53 +0700

    helper for determining the current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
 		base_title = "RichPig"
 		page_title.present? ? "#{page_title} | #{base_title}" : base_title
 	end
+
+	def nav_tab(title, url, options = {})
+		current_page = options.delete(:current_page)
+
+		css_class = current_page == title ? 'text-secondary' : 'text-white'
+
+		if options[:class].present?
+			options[:class] = options[:class] + ' ' + css_class
+		else
+			options[:class] = css_class
+		end
+
+		link_to title, url, options
+	end
+
+	def currently_at(current_page = "")
+		render partial: 'shared/menu', locals: {current_page: current_page}
+	end
 end
