ba3168b53d669675331e847f9b60492ea0114e56
ArBuZzZ44 <zahar1702@mail.ru>
2023-09-29 22:02:13 +0700

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 	include Pagy::Frontend
 
+	def pagination(obj)
+		raw(pagy_bootstrap_nav(obj)) if @pagy.pages > 1
+	end
+
 	def full_title(page_title = "")
 		base_title = "RichPig"
 		page_title.present? ? "#{page_title} | #{base_title}" : base_title
