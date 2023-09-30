aa49920f4c1ab91f59ebe38188144806f05f5e81
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-01 01:10:32 +0700

    added before filter for auth

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -14,6 +14,18 @@ module Authentication
 			@current_user = nil
 		end
 
+		def require_no_authentication
+			return if !user_signed_in?
+			flash[:warning] = "You are already signed in!"
+			redirect_to root_path
+		end
+
+		def require_authentication
+			return if user_signed_in?
+			flash[:warning] = "You are not signed in!"
+			redirect_to root_path
+		end
+
 		def current_user
 			@current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
 		end
