1b68b431a7c3c91aaad2eb2af5358accc7cacf86
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-19 03:57:07 +0700

    error handling

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,5 +3,14 @@ module Authorization
 
 	included do
 		include Pundit
+
+		rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+		private
+
+		def user_not_authorized
+			flash[:danger] = "You are not authorized for this action!"
+			redirect_to(request.preferer || root_path)
+		end
 	end
 end
