fd0bf31dc83fbb2ad633d454f25ac6d2017337de
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-22 04:37:00 +0700

    create policy for wallet

---
+++ app/policies/wallet_policy.rb
@@ -0,0 +1,21 @@
+class WalletPolicy < ApplicationPolicy
+	def index?
+    !user.guest?
+  end
+
+  def show?
+    user.author?(record)
+  end
+
+  def create?
+    !user.guest?
+  end
+
+  def update?
+    user.admin_role? || user.moderator_role? || user.author?(record)
+  end
+
+  def destroy?
+    user.admin_role? || user.author?(record)
+  end
+end
