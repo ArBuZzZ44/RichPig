ac258c03d635245395114899e06c1f487b688758
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-22 04:37:32 +0700

    update wallets controller

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,10 +1,15 @@
 class WalletsController < ApplicationController
+	before_action :set_wallet!, only: %i[show update edit destroy]
+	before_action :require_authentication
+	before_action :authorize_wallet!
+	after_action :verify_authorized
+
   def new
     @wallet = Wallet.new
   end
 
   def create
-    @wallet = Wallet.new wallet_params
+    @wallet = current_user.wallets.build wallet_params
 
     if @wallet.save
       flash[:success] = "A new budget has been successfully created"
@@ -15,8 +20,6 @@ class WalletsController < ApplicationController
   end
 
 	def show
-		@wallet = Wallet.find params[:id]
-
 		@operation = @wallet.operations.build
 		@pagy, @operations = pagy @wallet.operations.filter_operations(params[:filter]).order created_at: :desc
 
@@ -26,11 +29,10 @@ class WalletsController < ApplicationController
 		@balance_of_budget = @wallet[:budget] - @sum_of_expenses
 
 		@diff = (@wallet.end_date.to_date - Time.now.to_date).to_i
-		
 	end
 
   def index
-		@pagy, @wallets = pagy Wallet.order(created_at: :desc)
+		@pagy, @wallets = pagy Wallet.where(user_id: current_user.id).order(created_at: :desc)
   end
 
 	def destroy
@@ -41,4 +43,12 @@ class WalletsController < ApplicationController
   def wallet_params
     params.require(:wallet).permit(:budget, :start_date, :end_date)
   end
+
+	def set_wallet!
+		@wallet = Wallet.find params[:id]
+	end
+
+	def authorize_wallet!
+		authorize(@wallet || Wallet)
+	end
 end
