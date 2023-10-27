0bd3b456af17f56f89e5818077981723b3e1ffbb
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-27 16:01:05 +0700

    added method create

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,3 +1,8 @@
 class PasswordResetsController < ApplicationController
 	before_action :require_no_authentication
+
+	def create
+		PasswordResetMailer.with(user: @user).reset_email.deliver_later # отправит письмо в фоновом режиме, deliver_now отправит письмо сразу, но может долго грузить
+		
+	end
 end
