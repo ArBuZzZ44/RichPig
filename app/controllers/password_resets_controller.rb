89651acecfa51d6e61513d181182a7ab967a6cb7
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-31 23:22:36 +0700

    controller for reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,7 +2,15 @@ class PasswordResetsController < ApplicationController
 	before_action :require_no_authentication
 
 	def create
-		PasswordResetMailer.with(user: @user).reset_email.deliver_later # отправит письмо в фоновом режиме, deliver_now отправит письмо сразу, но может долго грузить
-		
+		@user = User.find_by(email: params[:email])
+
+		if @user.present?
+			@user.set_password_reset_token
+
+			PasswordResetMailer.with(user: @user).reset_email.deliver_later # отправит письмо в фоновом режиме, deliver_now отправит письмо сразу, но может долго грузить
+		end
+
+		flash[:success] = "password reset instructions have been sent to your email"
+		redirect_to new_session_path
 	end
 end
