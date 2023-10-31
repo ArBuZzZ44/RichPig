616b4d40dab073aecbfe8c7a176711eb10a0d286
ArBuZzZ44 <zahar1702@mail.ru>
2023-10-31 23:21:41 +0700

    add columns for reset token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_171040) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_082934) do
   create_table "additions", force: :cascade do |t|
     t.integer "balance"
     t.integer "capital_id", null: false
@@ -57,7 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_171040) do
     t.datetime "updated_at", null: false
     t.string "remember_token_digest"
     t.integer "role", default: 0, null: false
+    t.string "password_reset_token"
+    t.datetime "password_reset_token_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["password_reset_token"], name: "index_users_on_password_reset_token"
     t.index ["role"], name: "index_users_on_role"
   end
 
