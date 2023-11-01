6150422cf9003301fb7a696c9205ea45300420ff
ArBuZzZ44 <zahar1702@mail.ru>
2023-11-01 21:37:00 +0700

    add before action and edit method

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
 	before_action :require_no_authentication
+	before_action :set_user!, only: %i[edit update]
 
 	def create
 		@user = User.find_by(email: params[:email])
@@ -12,5 +13,21 @@ class PasswordResetsController < ApplicationController
 
 		flash[:success] = "password reset instructions have been sent to your email"
 		redirect_to new_session_path
+	end
+
+	def edit
+	end
+
+	def update
+
+	end
+
+	private
+
+	def set_user!
+		@user = User.find_by(email: params[:user][:email],
+												password_reset_token: params[:user][:password_reset_token])
+
+		redirect_to(new_session_path, flash: { warning: "password recovery failed" }) unless @user&.password_reset_period_valid?
 	end
 end
