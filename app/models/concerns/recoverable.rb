89d15ba18a8cc0832f83d9b9ecdc20d5c21036b0
ArBuZzZ44 <zahar1702@mail.ru>
2023-11-02 23:59:00 +0700

    clearing the token after reset

--- app/models/concerns/recoverable.rb
+++ app/models/concerns/recoverable.rb
@@ -2,10 +2,19 @@ module Recoverable
 	extend ActiveSupport::Concern
 
 	included do
+		# такая же функция обратного вызова, как и before_action
+		# т.к. в базе есть password_digest, то rails может проверить, изменялся ли он
+		before_update :clear_password_reset_token, if: :password_digest_changed?
+
     def set_password_reset_token
 			update password_reset_token: digest(SecureRandom.urlsafe_base64),
 					   password_reset_token_sent_at: Time.current
     end
+
+		def clear_password_reset_token # очищаем токен после сброса пароля
+			self.password_reset_token = nil
+			self.password_reset_token_sent_at = nil
+		end
 
 		def password_reset_period_valid?
 			password_reset_token_sent_at.present? && (Time.current - password_reset_token_sent_at) <= 60.minutes
